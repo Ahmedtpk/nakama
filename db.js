f13a0b0afb9f31831a5a2e9c04bcaffb9eb52052
Ahmed Ahmed <ahmedah@elkjop.no>
2022-08-22 17:25:07 +0200

    test adding databaseurl

--- db.js
+++ db.js
@@ -4,11 +4,20 @@ const pool = new Pool({
     user: process.env.USER,
     password: process.env.PASSWORD,
     database: process.env.DATABASE,
+    database_url: process.env.process.env.DATABASE,
     host: process.env.HOST,
     port: process.env.PORT,
     ssl: {
         rejectUnauthorized: false
       }
 })
+
+// const pool = new Pool({
+//   user: "postgres",
+//   password: "1999",
+//   database: "nakama",
+//   host: "localhost",
+//   port: 5432
+// })
 
 module.exports = pool
